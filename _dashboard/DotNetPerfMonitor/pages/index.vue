790cadcb16969fa4ef4d55377d4f93fe82462544
baimamboukar <baimamboukar@gmail.com>
2023-07-07 10:54:57 +0100

    updated tabs

--- _dashboard/DotNetPerfMonitor/pages/index.vue
+++ _dashboard/DotNetPerfMonitor/pages/index.vue
@@ -2,10 +2,9 @@
     <div class="flex flex-col px-4 py-4">
         <UtilsSearchBox />
         <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-            <el-tab-pane label="User" name="first">User</el-tab-pane>
-            <el-tab-pane label="Config" name="second">Config</el-tab-pane>
-            <el-tab-pane label="Role" name="third">Role</el-tab-pane>
-            <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+            <el-tab-pane label="F#" name="first">F#</el-tab-pane>
+            <el-tab-pane label="C#" name="third">C# Compiler</el-tab-pane>
+            <el-tab-pane label=".NET" name="fourth">.NET</el-tab-pane>
         </el-tabs>
 
 
