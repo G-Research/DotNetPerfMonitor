f4db35208c7825562e9be9700ec7ef3661e95452
baimamboukar <baimamboukar@gmail.com>
2023-07-07 10:53:36 +0100

    added tabs

--- _dashboard/DotNetPerfMonitor/nuxt.config.ts
+++ _dashboard/DotNetPerfMonitor/nuxt.config.ts
@@ -1,7 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/google-fonts", "@nuxthq/ui"],
+  modules: [
+    "@nuxtjs/google-fonts",
+    "@nuxthq/ui",
+    "@element-plus/nuxt",
+  ],
   googleFonts: {
     families: {
       Releway: true,
--- _dashboard/DotNetPerfMonitor/pages/index.vue
+++ _dashboard/DotNetPerfMonitor/pages/index.vue
@@ -1,33 +1,25 @@
 <template>
     <div class="flex flex-col px-4 py-4">
         <UtilsSearchBox />
-        <UDivider />
+        <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
+            <el-tab-pane label="User" name="first">User</el-tab-pane>
+            <el-tab-pane label="Config" name="second">Config</el-tab-pane>
+            <el-tab-pane label="Role" name="third">Role</el-tab-pane>
+            <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
+        </el-tabs>
 
-        <div
-            class="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
-            <ul class="flex flex-wrap -mb-px">
-                <li class="mr-2">
-                    <a href="#"
-                        class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">NuGet</a>
-                </li>
-                <li class="mr-2">
-                    <a href="#"
-                        class="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500"
-                        aria-current="page">F#</a>
-                </li>
-                <li class="mr-2">
-                    <a href="#"
-                        class="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">.NET
-                        SDK</a>
-                </li>
 
-            </ul>
-        </div>
-        <DashboardLineChart />
     </div>
 </template>
 
 
-<script setup>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import type { TabsPaneContext } from 'element-plus'
+
+const activeName = ref('first')
 const { metaSymbol } = useShortcuts()
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+    console.log(tab, event)
+}
 </script>
