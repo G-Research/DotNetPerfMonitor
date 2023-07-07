f7aef5bbd210557b5827ccc77c7889bbb0831126
baimamboukar <baimamboukar@gmail.com>
2023-07-07 15:40:48 +0100

    deploying with gh-cli

--- _dashboard/DotNetPerfMonitor/nuxt.config.ts
+++ _dashboard/DotNetPerfMonitor/nuxt.config.ts
@@ -1,16 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/google-fonts",
-    "@nuxthq/ui",
-    "@element-plus/nuxt",
-  ],
+  app: {
+    baseURL: "/DotNetPerfMonitor/_dashboard/DotNetPerfMonitor/",
+  },
+  modules: ["@nuxtjs/google-fonts", "@nuxthq/ui", "@element-plus/nuxt"],
   googleFonts: {
     families: {
       Releway: true,
     },
-    display: "swap",
   },
   css: ["@/assets/css/global.css"],
 });
