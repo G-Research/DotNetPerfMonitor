21bb918f100166690d4982015645e59a4c1f7f90
baimamboukar <baimamboukar@users.noreply.github.com>
2023-07-07 11:05:10 +0000

    changed push dir 129ee6b0c8e99620b9f33d07e37c6bc4b47ca0e7

--- _dashboard/DotNetPerfMonitor/dist/_nuxt/settings.02f05d02.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,$ as n,c as o,b as s,w as c,o as a,a as r}from"./entry.791dda0b.js";const _={},l=r("h1",null,"Settings",-1);function d(i,f){const e=n("center");return a(),o("div",null,[s(e,null,{default:c(()=>[l]),_:1})])}const m=t(_,[["render",d]]);export{m as default};
