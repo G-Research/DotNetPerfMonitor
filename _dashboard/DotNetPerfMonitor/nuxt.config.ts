8b0eb4d70cd5ac8726987d350d152ba664b1a93f
BAIMAM BOUKAR JEAN JACQUES <49169158+baimamboukar@users.noreply.github.com>
2023-07-18 21:15:49 +0100

    Integrated regressions page (#11)

--- _dashboard/DotNetPerfMonitor/nuxt.config.ts
+++ _dashboard/DotNetPerfMonitor/nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-
+  generate: {
+    routes: ["/notifications", "/settings", "/documentation", "/regressions"],
+  },
   colorMode: {
     preference: "dark",
   },
