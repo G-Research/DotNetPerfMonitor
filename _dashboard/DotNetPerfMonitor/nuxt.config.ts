b7cb7b8e5ebb9e76ae416abe846385ecb84766eb
baimamboukar <baimamboukar@gmail.com>
2023-07-25 06:38:50 +0100

    enable target

--- _dashboard/DotNetPerfMonitor/nuxt.config.ts
+++ _dashboard/DotNetPerfMonitor/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  //target: "static",
+  target: "static",
   app: {
     baseURL: "/DotNetPerfMonitor/",
   },
