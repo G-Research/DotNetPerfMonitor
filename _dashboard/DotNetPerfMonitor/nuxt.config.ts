e868e2a1da4550dc8e8bc0595721a82117bb3e67
baimamboukar <baimamboukar@gmail.com>
2023-07-25 06:56:08 +0100

    config format

--- _dashboard/DotNetPerfMonitor/nuxt.config.ts
+++ _dashboard/DotNetPerfMonitor/nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr: false,
   target: "static",
   app: {
     baseURL: "/DotNetPerfMonitor/",
