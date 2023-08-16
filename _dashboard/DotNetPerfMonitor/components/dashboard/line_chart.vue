93393682329381084ef3416b0cd9a0a5ae8d6122
baimamboukar <baimamboukar@gmail.com>
2023-08-16 16:23:09 +0100

    switch to new data source

--- _dashboard/DotNetPerfMonitor/components/dashboard/line_chart.vue
+++ _dashboard/DotNetPerfMonitor/components/dashboard/line_chart.vue
@@ -13,8 +13,8 @@
 const props = defineProps({
     scenario: String
 })
-const path = 'https://raw.githubusercontent.com/G-Research/DotNetPerfMonitor/main/data.csv'
-
+const path = "https://raw.githubusercontent.com/G-Research/DotNetPerfMonitor/main/data/nuget.csv"
+const file = '../../../../data/nuget.csv'
 const converted = await useCsvConverter(path)
 const scenario = useAlphaScenario()
 const filtered = useScenarioFilter(converted, props.scenario)
