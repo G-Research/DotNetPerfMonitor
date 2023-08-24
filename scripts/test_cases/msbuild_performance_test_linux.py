f6a8e9ae90037712ac623e5f52567681ffd2ba86
baimamboukar <baimamboukar@gmail.com>
2023-08-24 03:33:38 +0100

    add verbose logs

--- scripts/test_cases/msbuild_performance_test_linux.py
+++ scripts/test_cases/msbuild_performance_test_linux.py
@@ -20,8 +20,15 @@ TEST_REPO_NAME = "TestSolutions"
 TEST_SOLUTION_DIR = "LargeAppWithPrivatePackagesCentralisedNGBVRemoved/solution"
 
 
+def check_directory(event):
+    """Checking directory for debugging purposes"""
+    print(f'_____CHECKING DIR IN {event}________')
+    subprocess.call("ls", shell=True)
+
+
 def create_extract_destinations():
     """ Create the extract destination directories if they do not exist"""
+    check_directory('BEFORE CREATE EXTRACT DESTINATION')
     if not os.path.exists(EXTRACT_PATH):
         os.mkdir(EXTRACT_PATH)
     os.chdir(EXTRACT_PATH)
@@ -29,23 +36,32 @@ def create_extract_destinations():
         os.mkdir("base")
     if not os.path.exists("daily"):
         os.mkdir("daily")
+    check_directory('AFTER CREATE EXTRACT DESTINATION')
 
 
 def download_file(url, filename):
     """ Download file from url and save it to filename"""
+    check_directory('BEFORE DOWNLOAD FILE')
     with urllib.request.urlopen(url) as response, open(filename, 'wb') as out_file:
         data = response.read()
         out_file.write(data)
+    check_directory('AFTER DOWNLOAD FILE')
 
 
 def download_and_extract_dotnet_sdk(version_url, is_base):
-    path = f"{EXTRACT_PATH}/base" if is_base else f"{EXTRACT_PATH}/daily"
+
+    os.chdir(EXTRACT_PATH)
+
+    path = "base" if is_base else "daily"
     tar_gz_file = "dotnet-sdk.tar.gz"
     download_file(version_url, tar_gz_file)
 
     # Extract the tar.gz file
+    check_directory('BEFORE EXTRACT FILE')
     extract_command = f"tar -xzf {tar_gz_file} -C {path}"
     subprocess.call(extract_command, shell=True)
+
+    os.chdir("..")
 
 
 def measure_execution_time(command):
@@ -58,9 +74,9 @@ def measure_execution_time(command):
         [Number]: [ellapsed time in seconds]
     """
 
-    subprocess.call("ls", shell=True)
+    check_directory('BEFORE MEASURE EXECUTION TIME')
     subprocess.call(f"cd {TEST_REPO_NAME}", shell=True)
-    subprocess.call("ls", shell=True)
+    check_directory('AFTER CHECKINT OUT TO REPO')
 
     # Record start time
     start_time = time.time()
@@ -82,11 +98,11 @@ def clone_repository(repo_url, repo_path):
         repo_url (String): url of the repository to be cloned
         repo_path (String): path containing test code
     """
-    subprocess.call("ls", shell=True)
+    check_directory('BEFORE CLONE REPO')
     # Clone the repository containing the solution
     subprocess.call(f"git clone {repo_url}", shell=True)
-    subprocess.call(f"cd {TEST_REPO_NAME}/{repo_path}", shell=True)
-    subprocess.call("ls", shell=True)
+
+    check_directory('AFTER CLONE REPO')
 
 
 def delete_clone(repo_url):
@@ -118,6 +134,8 @@ def main():
 
     # clone the repository and navigate to the solution directory
     clone_repository(TEST_SOLUTION_REPO_URL, TEST_SOLUTION_DIR)
+
+    check_directory('BEFORE BUILD SOLUTION')
 
     # build the solution using the base version
     exec_path = "../../sdk/base/dotnet"
