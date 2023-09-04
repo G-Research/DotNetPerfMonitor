219d23af6d0dd801352e479bc5cb5f7ffe94ca7b
baimamboukar <baimamboukar@gmail.com>
2023-09-04 03:00:40 +0100

    added restore to linux

--- scripts/test_cases/large_app_with_private_packages_centralised_ngbv_removed_windows.py
+++ scripts/test_cases/large_app_with_private_packages_centralised_ngbv_removed_windows.py
@@ -58,6 +58,7 @@ def run_build_to_restore_packages(dotnet_executable):
     Args:
         dotnet_executable (_type_): _description_
     """
+    subprocess.run([dotnet_executable, 'restore'], check=True)
     subprocess.run([dotnet_executable, 'build'], check=True)
 
 
