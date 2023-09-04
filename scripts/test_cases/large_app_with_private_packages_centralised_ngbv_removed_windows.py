4f0ab518bd393d15df4698f9f381e2b4ef61a0f7
baimamboukar <baimamboukar@gmail.com>
2023-09-04 04:03:28 +0100

    change msbuild csv path and rm build step

--- scripts/test_cases/large_app_with_private_packages_centralised_ngbv_removed_windows.py
+++ scripts/test_cases/large_app_with_private_packages_centralised_ngbv_removed_windows.py
@@ -59,7 +59,7 @@ def run_build_to_restore_packages(dotnet_executable):
         dotnet_executable (_type_): _description_
     """
     subprocess.run([dotnet_executable, 'restore'], check=True)
-    subprocess.run([dotnet_executable, 'build'], check=True)
+    # subprocess.run([dotnet_executable, 'build'], check=True)
 
 
 def measure_execution_time(command):
