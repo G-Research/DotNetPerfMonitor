4e0433fa12a9eadaa0c73d8b76bb394acdf49923
Marcin Krystianc <marcin.krystianc@gmail.com>
2023-11-16 09:43:09 +0100

    NuGetAuditLevel=low?

--- scripts/perftests/testCases/OrchardCore-5dbd92c.ps1
+++ scripts/perftests/testCases/OrchardCore-5dbd92c.ps1
@@ -19,9 +19,8 @@ $ProgressPreference = 'SilentlyContinue' #https://github.com/PowerShell/PowerShe
 if ($globalJsonPath) {Remove-Item "$sourcePath\$globalJsonPath"}
 
 #########################################################
-# https://github.com/G-Research/NuPerfMonitor/issues/8
 # Workaround for errors due to a vulnerability scanning (https://github.com/NuGet/Home/blob/dev/proposed/2022/vulnerabilities-in-restore.md)
-New-Item "$sourcePath\Directory.Build.rsp" -Value "/p:NuGetAudit=disable"
+New-Item "$sourcePath\Directory.Build.rsp" -Value "/p:NuGetAuditLevel=low"
 
 $versions = @("dotnet_base", "dotnet")
 ForEach ($version In $versions) {
